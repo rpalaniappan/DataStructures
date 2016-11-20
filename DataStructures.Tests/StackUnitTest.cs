c683f812b5ae7c65b798b54e6710294414045dc9
Pal Ramanathan <rpalaniappan@hotmail.com>
2016-11-20 18:21:48 -0500

    Unit tests

--- DataStructures.Tests/StackUnitTest.cs
+++ DataStructures.Tests/StackUnitTest.cs
@@ -7,8 +7,29 @@ namespace DataStructures.Tests
     public class StackUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void StackTest()
         {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            Assert.IsTrue(stack.Length==2);
+            Assert.IsTrue(stack.Pop()==2);
+            Assert.IsTrue(stack.Peek() == 1);
+            Assert.IsTrue(stack.Pop() == 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void StackTest_ThrowsException()
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            Assert.IsTrue(stack.Length == 2);
+            Assert.IsTrue(stack.Pop() == 2);
+            Assert.IsTrue(stack.Peek() == 1);
+            Assert.IsTrue(stack.Pop() == 1);
+            stack.Pop();
         }
     }
 }
